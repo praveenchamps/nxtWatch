9d4e1c2400068074f7765b17654161faf8d10f16
praveenchamps <praveenchamps100@gmail.com>
2024-04-12 17:40:37 +0000

    Your commit message

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,23 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
 
 import './index.css'
 
 class Login extends Component {
-  state = {username: '', password: '', showPassword: false, showError: false}
+  state = {
+    username: '',
+    password: '',
+    showPassword: false,
+    showError: false,
+    errorMsg: '',
+  }
 
-  onChangeUser = event => {
+  onChangeUserName = event => {
     this.setState({username: event.target.value})
   }
 
-  onChangePass = event => {
+  onChangePassword = event => {
     this.setState({password: event.target.value})
   }
 
@@ -24,8 +31,8 @@ class Login extends Component {
     this.setState({showError: true, errorMsg})
   }
 
-  onSubmit = async e => {
-    e.preventDefault()
+  onSubmit = async event => {
+    event.preventDefault()
     const {username, password} = this.state
     const UserDetails = {username, password}
     const url = 'https://apis.ccbp.in/login'
@@ -35,67 +42,75 @@ class Login extends Component {
     }
     const response = await fetch(url, options)
     const data = await response.json()
-    if (data.ok === true) {
+    if (response.ok === true) {
       this.onSubmitSuccess(data.jwt_token)
     } else {
       this.onSubmitFailure(data.error_msg)
     }
   }
 
-  renderUsername = () => {
-    const {username} = this.state
-    return (
-      <>
-        <label htmlFor="user" className="label">
-          USERNAME
-        </label>
-        <input
-          id="user"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={this.onChangeUser}
-          className="input"
-        />
-      </>
-    )
-  }
-
-  renderPassword = () => {
-    const {password} = this.state
-    return (
-      <>
-        <label htmlFor="pass" className="label">
-          PASSWORD
-        </label>
-        <input
-          id="pass"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={this.onChangePass}
-          className="input"
-        />
-      </>
-    )
+  onShowPassword = () => {
+    this.setState(prevState => ({showPassword: !prevState.showPassword}))
   }
 
   render() {
-    const {showPassword, showError} = this.state
+    const {showPassword, showError, username, password, errorMsg} = this.state
+    const typeText = showPassword ? 'text' : 'password'
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken === undefined) {
+      return <Redirect to="/" />
+    }
     return (
       <div className="main-con">
         <div className="login-con">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-            alt="watch logo"
-            className="log-img"
-          />
+          <div className="div-img">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+              alt="watch logo"
+              className="log-img"
+            />
+          </div>
           <form onSubmit={this.onSubmit} className="form">
-            <div className="input-container">{this.renderUsername()}</div>
-            <div className="input-container">{this.renderPassword()}</div>
+            <div className="input-container">
+              <label htmlFor="user" className="label">
+                USERNAME
+              </label>
+              <input
+                id="user"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={this.onChangeUserName}
+                className="input"
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="pass" className="label">
+                PASSWORD
+              </label>
+              <input
+                id="pass"
+                type={typeText}
+                placeholder="Password"
+                value={password}
+                onChange={this.onChangePassword}
+                className="input"
+              />
+            </div>
+            <div className="check-box">
+              <input
+                type="checkbox"
+                id="checkbox"
+                onChange={this.onShowPassword}
+              />
+              <label htmlFor="checkbox" className="label">
+                Show Password
+              </label>
+            </div>
             <button type="submit" className="button-login">
               Login
             </button>
+            {showError && <p className="error">*{errorMsg}</p>}
           </form>
         </div>
       </div>
